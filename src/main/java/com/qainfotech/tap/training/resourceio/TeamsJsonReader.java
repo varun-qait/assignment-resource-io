31de7dd15aac379e979878f47037ade8feda6ac7
Varun Aggarwal <varunaggarwal1995@gmail.com>
2017-06-28 10:35:10 +0530

    Update TeamsJsonReader.java

--- src/main/java/com/qainfotech/tap/training/resourceio/TeamsJsonReader.java
+++ src/main/java/com/qainfotech/tap/training/resourceio/TeamsJsonReader.java
@@ -3,7 +3,21 @@ package com.qainfotech.tap.training.resourceio;
 import com.qainfotech.tap.training.resourceio.exceptions.ObjectNotFoundException;
 import com.qainfotech.tap.training.resourceio.model.Individual;
 import com.qainfotech.tap.training.resourceio.model.Team;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 /**
  *
@@ -16,9 +30,54 @@ public class TeamsJsonReader{
      * 
      * @return 
      */
-    public List<Individual> getListOfIndividuals(){
-        throw new UnsupportedOperationException("Not implemented.");
+    
+    List<Individual> inactive;
+    List<Individual> active;
+
+    JSONObject obj;
+    void jsonParser() throws IOException {
+        try {
+            JSONParser jp = new JSONParser();
+            FileReader reader = new FileReader(new File("C:\\Users\\varunaggarwal\\Documents\\NetBeansProjects\\assignment-resource-io-master\\src\\main\\resources\\db.json"));
+            obj = (JSONObject) jp.parse(reader);
+        } catch (FileNotFoundException | ParseException ex) {
+            Logger.getLogger(TeamsJsonReader.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
+    
+    List<Individual> ind;
+    List<Team> t;
+    public List<Individual> getListOfIndividuals(){
+        ind = new ArrayList<>();
+        try {
+            jsonParser();
+        } catch(Exception e) {
+        
+        }
+        JSONArray arr = (JSONArray) obj.get("individuals"); // here you get an array of individuals
+        for(int i = 0; i < arr.size(); i++) {
+            JSONObject iobj = (JSONObject) arr.get(i); // each entry in jsonArray is itself considered as a jsonobject
+            //System.out.println(iobj.toString());
+            Map<String, Object> map = new HashMap<>();
+            //System.out.println(iobj.toString()); // {"name":"John Doe","active":true,"id":1201}
+            //map.put(iobj.toString(), iobj); // Here i am puting the toString() representation of whole object as key -- WRONG APPROACH
+            //map.put(String.valueOf(iobj.get("id")), iobj); // This is wrong as here i am using value of id as key
+            map.put("id", iobj);
+            Individual individual = new Individual(map);
+            //System.out.println(i + " is added\n\n");
+            
+            
+            ind.add(individual);
+        }
+        return ind;
+    }
+    
+                                                    // Debug Code
+                                                    void printArrayListSize() {
+                                                        System.out.println(ind.size());
+
+                                                    }
+    
     
     /**
      * get individual object by id
@@ -28,7 +87,14 @@ public class TeamsJsonReader{
      * @throws com.qainfotech.tap.training.resourceio.exceptions.ObjectNotFoundException 
      */
     public Individual getIndividualById(Integer id) throws ObjectNotFoundException{
-        throw new UnsupportedOperationException("Not implemented.");
+        ind = getListOfIndividuals();
+        //System.out.println("size of ind " + ind.size());
+        for(int i = 0; i < ind.size(); i++) {
+            if(ind.get(i).getId() == id.intValue()) {
+                return ind.get(i);
+            }
+        }
+        throw new ObjectNotFoundException("Object not found", "id", id.toString());
     }
     
     /**
@@ -39,7 +105,15 @@ public class TeamsJsonReader{
      * @throws com.qainfotech.tap.training.resourceio.exceptions.ObjectNotFoundException 
      */
     public Individual getIndividualByName(String name) throws ObjectNotFoundException{
-        throw new UnsupportedOperationException("Not implemented.");
+        ind = getListOfIndividuals();
+        
+        for(int i = 0; i < ind.size(); i++) {
+            if((ind.get(i).getName()).equals(name)) {
+                return ind.get(i);
+            }
+        }
+        throw new ObjectNotFoundException("Individual not found", "name", name);
+        //throw new UnsupportedOperationException("Not implemented.");
     }
     
     
@@ -49,7 +123,15 @@ public class TeamsJsonReader{
      * @return List of inactive individuals object
      */
     public List<Individual> getListOfInactiveIndividuals(){
-        throw new UnsupportedOperationException("Not implemented.");
+        inactive = new ArrayList<>();
+        ind = getListOfIndividuals();
+        for(int i = 0; i < ind.size(); i++) {
+            if(!(ind.get(i).isActive())) {
+                inactive.add(ind.get(i));
+            }
+        }
+        return inactive;
+        //throw new UnsupportedOperationException("Not implemented.");
     }
     
     /**
@@ -58,7 +140,15 @@ public class TeamsJsonReader{
      * @return List of active individuals object
      */
     public List<Individual> getListOfActiveIndividuals(){
-        throw new UnsupportedOperationException("Not implemented.");
+        active = new ArrayList<>();
+        ind = getListOfIndividuals();
+        for(int i = 0; i < ind.size(); i++) {
+            if(ind.get(i).isActive()) {
+                active.add(ind.get(i));
+            }
+        }
+        return active;
+        //throw new UnsupportedOperationException("Not implemented.");
     }
     
     /**
@@ -67,6 +157,30 @@ public class TeamsJsonReader{
      * @return 
      */
     public List<Team> getListOfTeams(){
-        throw new UnsupportedOperationException("Not implemented.");
+        try {
+            jsonParser();
+        } catch (IOException ex) {
+            Logger.getLogger(TeamsJsonReader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        t = new ArrayList<>();
+        JSONArray team = (JSONArray) obj.get("teams");
+        for(int i = 0; i < team.size(); i++) {
+            JSONObject tobj = (JSONObject) team.get(i);
+            Map<String, Object> map = new HashMap<>();
+            map.put("id", tobj);
+            Team teamObj = new Team(map);
+            t.add(teamObj);
+        }
+        return t;
+        //throw new UnsupportedOperationException("Not implemented.");
     }
+    
+    
+//    public static void main(String[] args) {
+//        TeamsJsonReader tjr = new TeamsJsonReader();
+//        //tjr.getListOfIndividuals();
+//        //tjr.printArrayListSize();
+//        tjr.getListOfTeams();
+//    }
+    
 }
