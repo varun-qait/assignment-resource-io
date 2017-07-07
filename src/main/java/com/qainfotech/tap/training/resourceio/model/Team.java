bfed57821e95a7a04bc5802531528900d3b70057
Varun Aggarwal <varunaggarwal1995@gmail.com>
2017-07-07 10:06:55 +0530

    Updated Team.java

--- src/main/java/com/qainfotech/tap/training/resourceio/model/Team.java
+++ src/main/java/com/qainfotech/tap/training/resourceio/model/Team.java
@@ -6,100 +6,127 @@ import java.util.Map;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import com.qainfotech.tap.training.resourceio.TeamsJsonReader;
+import java.util.HashMap;
 
 /**
  *
  * @author Ramandeep RamandeepSingh AT QAInfoTech.com
  */
 public class Team {
-    
+
     private final String name;
     private final Integer id;
     private final List<Individual> members;
     TeamsJsonReader tjr = new TeamsJsonReader();
     List<Individual> activeTeam;
     List<Individual> inActiveTeam;
+
     
-    public Team(Map<String, Object> teamMap){
-        JSONObject team = (JSONObject) teamMap.get("id"); // In db.json there are 2 objects. Returns a team object.
-        this.id = Integer.parseInt(team.get("id").toString());
-        this.name = team.get("name").toString(); 
+    public Team(Map<String, Object> teamMap) {
+
+//        JSONObject team = (JSONObject) teamMap.get("id"); // In db.json there are 2 objects. Returns a team object.
+//        this.id = Integer.parseInt(team.get("id").toString());
+//        this.name = team.get("name").toString(); 
+//        this.members = new ArrayList<>();
+//        JSONArray memberJsonArray = (JSONArray) team.get("members"); // Getting the reference to 'members' field of a team object and as it is a list i am casting it to a jsonarray 
+//        List<Individual> ind = tjr.getListOfIndividuals(); // Creating a local copy of individual list.
+//        
+//        for(int i = 0; i < memberJsonArray.size(); i++) {
+//            //the reason why the below statement will not run is that in our json we don't always have id first attribute.
+//            //int employeeId = (Integer) memberJsonArray.get(i);
+//            Long value = (Long) memberJsonArray.get(i);
+//            int employeeId = (int) value.longValue();
+//            for(int j = 0; j < ind.size(); j++) {
+//                if(ind.get(j).getId() == employeeId) {
+//                    this.members.add(ind.get(j));
+//                }
+//            }
+//        }
+
+
+        this.id = Integer.parseInt(teamMap.get("id").toString());
+        this.name = teamMap.get("name").toString();
         this.members = new ArrayList<>();
-        JSONArray memberJsonArray = (JSONArray) team.get("members"); // Getting the reference to 'members' field of a team object and as it is a list i am casting it to a jsonarray 
+        List memberList = (List) teamMap.get("members"); // Getting the reference to 'members' field of a team object and as it is a list i am casting it to a jsonarray 
         List<Individual> ind = tjr.getListOfIndividuals(); // Creating a local copy of individual list.
+
         
-        for(int i = 0; i < memberJsonArray.size(); i++) {
-            //the reason why the below statement will not run is that in our json we don't always have id first attribute.
-            //int employeeId = (Integer) memberJsonArray.get(i);
-            Long value = (Long) memberJsonArray.get(i);
-            int employeeId = (int) value.longValue();
-            for(int j = 0; j < ind.size(); j++) {
-                if(ind.get(j).getId() == employeeId) {
+        for (int i = 0; i < memberList.size(); i++) {
+            for (int j = 0; j < ind.size(); j++) {
+                if (ind.get(j).getId() == Integer.parseInt(memberList.get(i).toString())) {
+                    //if(ind.get(j).getId() == employeeId) {
                     this.members.add(ind.get(j));
+
                 }
             }
         }
-        //throw new UnsupportedOperationException("Not implemented.");
     }
-    
+
     /**
      * get team name
-     * 
-     * @return 
+     *
+     * @return
      */
-    public String getName(){
+    public String getName() {
         return name;
     }
-    
+
     /**
      * get team id
-     * 
-     * @return 
+     *
+     * @return
      */
-    public Integer getId(){
+    public Integer getId() {
         return id;
     }
-    
-    /** 
+
+    /**
      * get list of individuals that are members of this team
-     * 
-     * @return 
+     *
+     * @return
      */
-    public List<Individual> getMembers(){
+    public List<Individual> getMembers() {
         return members;
     }
-    
+
     /**
-     * get a list of individuals that are members of this team and are also active
-     * 
-     * @return 
+     * get a list of individuals that are members of this team and are also
+     * active
+     *
+     * @return
      */
-    public List<Individual> getActiveMembers(){
+    public List<Individual> getActiveMembers() {
         activeTeam = new ArrayList<>();
-        for(int i = 0; i < members.size(); i++) {
-            if(members.get(i).isActive()) {
+        for (int i = 0; i < members.size(); i++) {
+            if (members.get(i).isActive()) {
                 activeTeam.add(members.get(i));
             }
         }
-        
-        return activeTeam;     
+
+        return activeTeam;
         //throw new UnsupportedOperationException("Not implemented.");
     }
-        
+
     /**
      * get a list of individuals that are members of this team but are inactive
-     * 
-     * @return 
+     *
+     * @return
      */
-    public List<Individual> getInactiveMembers(){
+    public List<Individual> getInactiveMembers() {
         inActiveTeam = new ArrayList<>();
-        for(int i = 0; i < members.size(); i++) {
-            if(!(members.get(i).isActive())) {
+        for (int i = 0; i < members.size(); i++) {
+            if (!(members.get(i).isActive())) {
                 inActiveTeam.add(members.get(i));
             }
         }
-        
-        return inActiveTeam; 
+
+        return inActiveTeam;
         // throw new UnsupportedOperationException("Not implemented.");
     }
+
+    @Override
+    public String toString() {
+        return "Team{" + "name=" + name + ", id=" + id + ", members=" + members + ", tjr=" + tjr + ", activeTeam=" + activeTeam + ", inActiveTeam=" + inActiveTeam + '}';
+    }
+
 }
